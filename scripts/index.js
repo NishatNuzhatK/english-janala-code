ea1f7de05c5e17ebac915ffa03c65c0278f9a556
NishatNuzhatK <nishat.nuzhat.k@gmail.com>
2026-03-04 07:27:17 +0600

    added condition for null values

--- scripts/index.js
+++ scripts/index.js
@@ -38,11 +38,13 @@ const displayLevelWord = (words) =>{
 
     words.forEach((word) => {
         const card = document.createElement('div');
+
+        // 6 for null values conditinal rendaring
         card.innerHTML = `
            <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
-            <h2 class="font-bold text-2xl">${word.word}</h2>
+            <h2 class="font-bold text-2xl">${word.word? word.word : "শব্দ পাওয়া যায়নি"}</h2>
             <p class="font-semibold ">Meaning / pronumciation</p>
-            <div class="font-bangla text-2xl font-medium text-gray-700">"${word.meaning} / ${word.pronunciation}"</div>
+            <div class="font-bangla text-2xl font-medium text-gray-700">"${word.meaning? word.meaning : "অর্থ পাওয়া যায়নি" } / ${word.pronunciation? word.pronunciation : "Pronunciation পাওয়া যায়নি"}"</div>
             <div class="flex justify-between items-center">
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80] "><i class="fa-solid fa-circle-info"></i></button>
                 <button class="btn  bg-[#1A91FF10] hover:bg-[#1A91FF80] "><i class="fa-solid fa-volume-high"></i></button>
